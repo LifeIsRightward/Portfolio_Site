fab28f0290856fe3395f2bed7fc70ccaf1aca307
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2023-08-13 18:58:37 +0900

    JS - ArrowUp

--- Dream_Coding(PortFolio)/fortpolio/src/main.js
+++ Dream_Coding(PortFolio)/fortpolio/src/main.js
@@ -36,4 +36,14 @@ document.addEventListener('scroll', ()=>{
     home.style.opacity = 1 - (window.scrollY / homeHeight);
 });
 
+// Arrowup버튼을 아래로 스크롤시 투명하게 처리함.
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.style.opacity = 1;
+    }else{
+        arrowUp.style.opacity = 0;
+    }
+});
+
 
